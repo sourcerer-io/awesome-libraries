afcbd74f60996b93f93e67166304373bb2facf6a
Alexander Surkov <surkov.alexander@gmail.com>
2019-03-11 16:55:56 -0400

    java libs; add ignore-repos list; adjust libriesio script to process Maven libs

---
+++ sync/ignore-repos.js
@@ -0,0 +1,10 @@
+(async function() {
+  'use strict';
+
+module.exports = [
+  "http://junit.cvs.sourceforge.net/junit", // java.junit, junit-team/junit4
+  "http://cglib.cvs.sourceforge.net/cglib", // java.cglib, cglib/cglib
+  "AlibabaTech/fastjson", // java.fastjson, alibaba/fastjson
+];
+
+}());
