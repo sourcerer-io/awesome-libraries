6ce62d81cf4dc75cd67b86681d2635e216683dd3
Alexander Surkov <surkov.alexander@gmail.com>
2019-03-11 21:13:00 -0400

    fix

--- sync/ignore-repos.js
+++ sync/ignore-repos.js
@@ -5,6 +5,7 @@ module.exports = [
   "http://junit.cvs.sourceforge.net/junit", // java.junit, junit-team/junit4
   "http://cglib.cvs.sourceforge.net/cglib", // java.cglib, cglib/cglib
   "AlibabaTech/fastjson", // java.fastjson, alibaba/fastjson
+  "clojure/clojure", // not a library
 ];
 
 }());
