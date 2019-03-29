7ff64e0aa1efb135321c43eb212d500ad05f4e3a
Alexander Surkov <surkov.alexander@gmail.com>
2019-03-29 08:25:34 -0400

    perl libs

--- sync/ignore-repos.js
+++ sync/ignore-repos.js
@@ -5,9 +5,10 @@ module.exports = [
   "http://junit.cvs.sourceforge.net/junit", // java.junit, junit-team/junit4
   "http://cglib.cvs.sourceforge.net/cglib", // java.cglib, cglib/cglib
   "AlibabaTech/fastjson", // java.fastjson, alibaba/fastjson
-  "clojure/clojure", // not a library
-  "tokuhirom/Minilla", // not a library, perl tool
-  "miyagawa/Dist-Milla", // not a library, perl tool
+  "clojure/clojure", // programming language
+  "tokuhirom/Minilla", // perl tool
+  "miyagawa/Dist-Milla", // perl tool
+  "rjbs/Dist-Zilla", // perl tool
   "sphinx-doc/sphinx", // a python tool
   "pypa/setuptools", // a python tool
   "https://svn.apache.org/repos/asf/xerces/java", // already supported at different repo, see java.xerces
